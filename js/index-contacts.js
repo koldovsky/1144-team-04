178b020dafe3315e3a1ff366dffbbd31a42db43c
Maksym Khakimzhanov <max.hakimzhanov@gmail.com>
2024-02-19 00:40:53 +0200

    Updated index.contacts.css and index-cintacts.js

--- js/index-contacts.js
+++ js/index-contacts.js
@@ -1,6 +1,7 @@
 const root = document.querySelector(":root");
 const body = document.querySelector("body");
 const modal = document.querySelector(".contacts__form-container");
+const popUpWrapper = document.querySelector(".contacts__pop-up-wrapper");
 const openButton = document.querySelector(".contacts__open-button");
 const closeButton = document.querySelector(".contacts__close-button");
 let scrollPosition;
@@ -10,11 +11,13 @@ openButton.addEventListener("click", () => {
     root.classList.toggle("responsive");
     body.classList.toggle("fixed");
     modal.classList.toggle("responsive");
+    popUpWrapper.classList.toggle("responsive");
 })
 
 closeButton.addEventListener("click", () => {
     root.classList.remove("responsive");
     body.classList.remove("fixed");
     modal.classList.remove("responsive");
+    popUpWrapper.classList.remove("responsive");
     window.scrollTo(0, scrollPosition);
 })
