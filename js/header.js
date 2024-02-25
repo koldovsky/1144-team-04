dd2463fd5d9807c3704ebd7f7fb0bd6dce203629
YuliaRomanyk <117859473+YulichkaR@users.noreply.github.com>
2024-02-25 01:06:30 +0000

    Added burger menu and modal window using js

---
+++ js/header.js
@@ -0,0 +1,36 @@
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const btnSub = document.querySelector(".header__menu-btn");
+const navLinks = document.querySelector(".header__nav-menu");
+const openModalBtn = document.querySelector(".header__button");
+const modalContainer = document.querySelector(".modal-container");
+const overlay = document.querySelector(".overlay");
+const closeModalBtn = document.querySelector(".btn-close");
+
+btnSub.addEventListener("click", () => {
+    header.classList.toggle("responsive");
+})
+
+navLinks.addEventListener("click", () => {
+    header.classList.remove("responsive");
+})
+
+
+const openModal = function () {
+    modalContainer.classList.remove("hidden");
+};
+
+openModalBtn.addEventListener("click", openModal);
+
+const closeModal = function () {
+    modalContainer.classList.add("hidden");
+};
+
+closeModalBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" && !modalContainer.classList.contains("hidden")) {
+        closeModal();
+    }
+});
--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 document.addEventListener("partialsLoaded", () => {
+  import("./header.js");
   import("./index-contacts.js");
   import("./index-our-classes.js");
 });
