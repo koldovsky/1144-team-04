52d933f0274fb3bef65cd20bfd3078ed3f82f654
Katherinachub <156336877+Katherinachub@users.noreply.github.com>
2024-02-26 14:26:50 +0000

    Delete animation

--- js/index.latest-articles.js
+++
@@ -1,7 +0,0 @@
-
-  
-  const bubblyButtons = document.getElementsByClassName("buttom__class");
-  
-  for (const i = 0; i < bubblyButtons.length; i++) {
-    bubblyButtons[i].addEventListener('click', animateButton, false);
-  }
