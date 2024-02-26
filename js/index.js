4da8f6276096126d55d74bde7b9d83fe150052dc
Katherinachub <156336877+Katherinachub@users.noreply.github.com>
2024-02-26 13:08:01 +0000

    Start js

--- js/index.js
+++ js/index.js
@@ -2,5 +2,6 @@ document.addEventListener("partialsLoaded", () => {
   import("./header.js");
   import("./index-contacts.js");
   import("./index-our-classes.js");
-  import("./index-gallery.js")
+  import("./index-gallery.js");
+  import("./index.latest-articles.js");
 });
