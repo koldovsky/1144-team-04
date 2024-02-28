bf8694f16ed315e5e9f2b2362c2d6399c16cb84e
Maksym Khakimzhanov <max.hakimzhanov@gmail.com>
2024-02-28 15:24:02 +0200

    Updated index.contacts.css, index-contacts.js, index.contacts.html

--- js/index-contacts.js
+++ js/index-contacts.js
@@ -2,16 +2,39 @@ const body = document.querySelector("body");
 const modal = document.querySelector(".contacts__form-container");
 const openButton = document.querySelector(".contacts__open-button");
 const closeButton = document.querySelector(".contacts__close-button");
-const wrapper = document.querySelector(".contacts__pop-up-wrapper")
+const wrapper = document.querySelector(".contacts__pop-up-wrapper");
 
 openButton.addEventListener("click", () => {
-    body.classList.toggle("fixed");
-    modal.classList.toggle("responsive");
-    wrapper.classList.toggle("responsive");
-})
+  body.classList.toggle("fixed");
+  modal.classList.toggle("responsive");
+  wrapper.classList.toggle("responsive");
+});
 
 closeButton.addEventListener("click", () => {
-    body.classList.remove("fixed");
-    modal.classList.remove("responsive");
-    wrapper.classList.remove("responsive");
-})
+  body.classList.remove("fixed");
+  modal.classList.remove("responsive");
+  wrapper.classList.remove("responsive");
+});
+
+const form = document.querySelector(".contacts__form");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  const status = document.querySelector(".contacts__form-status");
+  const response = await fetch(event.target.action, {
+    method: form.method,
+    body: new FormData(event.target),
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  if (response.ok) {
+    status.innerText = `Your data was successfully sent! 
+                        Wait, the manager will contact you soon`;
+    form.reset();
+  } else {
+    status.innerText = "An error has occurred!";
+  }
+}
+
+form.addEventListener("submit", handleSubmit);
