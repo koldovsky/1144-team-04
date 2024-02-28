9529fc2d6b3bd16a563f2b633e1f5003fc10fdce
Maksym Khakimzhanov <max.hakimzhanov@gmail.com>
2024-02-28 03:50:15 +0200

    Updated index.contacts.css, index-contacts.js, index.contacts.html and  Fixed global.css

--- js/index-contacts.js
+++ js/index-contacts.js
@@ -1,23 +1,17 @@
-const root = document.querySelector(":root");
 const body = document.querySelector("body");
 const modal = document.querySelector(".contacts__form-container");
-const popUpWrapper = document.querySelector(".contacts__pop-up-wrapper");
 const openButton = document.querySelector(".contacts__open-button");
 const closeButton = document.querySelector(".contacts__close-button");
-let scrollPosition;
+const wrapper = document.querySelector(".contacts__pop-up-wrapper")
 
 openButton.addEventListener("click", () => {
-    scrollPosition = window.scrollY;
-    root.classList.toggle("responsive");
     body.classList.toggle("fixed");
     modal.classList.toggle("responsive");
-    popUpWrapper.classList.toggle("responsive");
+    wrapper.classList.toggle("responsive");
 })
 
 closeButton.addEventListener("click", () => {
-    root.classList.remove("responsive");
     body.classList.remove("fixed");
     modal.classList.remove("responsive");
-    popUpWrapper.classList.remove("responsive");
-    window.scrollTo(0, scrollPosition);
+    wrapper.classList.remove("responsive");
 })
