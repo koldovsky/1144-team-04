80423fdfb2e7dd6d736faed18bf0946e5d307929
nazar-r <157609345+nazar-r@users.noreply.github.com>
2024-03-06 18:07:28 +0000

    Update Footer's Items

--- js/footer.js
+++ js/footer.js
@@ -1,7 +1,21 @@
-let button = document.querySelectorAll('.item-content__heading')
+const form = document.getElementById("contact-form");
 
-button.forEach((btn) =>
-    btn.addEventListener("click", () => {
-        btn.parentElement.classList.toggle("activited");
+async function handleSubmit(event) {
+    event.preventDefault();
+    const status = document.getElementById("contact-form-status");
+    const response = await fetch(event.target.action, {
+        method: form.method,
+        body: new FormData(event.target),
+        headers: {
+            'Accept': 'application/json'
+        }
     })
-);
+    if (response.ok) {
+            status.innerHTML = "Thanks for your subcription!";
+            form.reset()
+    } else {
+        status.innerText = 'There was an error!';
+    };
+}
+
+form.addEventListener("submit", handleSubmit);
